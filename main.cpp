fb7ef5be175aa94c02f822cc8556c181c4086076
K024G <K024G@MSI>
2025-04-15 10:33:57 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+
+// windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	// 出力ウィンドウへの文字出力
+	OutputDebugStringA("Hellp,DirectX!\n");
+
+	return 0;
+
+
+
+}
