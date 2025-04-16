7ecdd074fb6c69bdf3b50d6ca0523371ccb44cf6
takeuti2319 <k024g0068@g.neec.ac.jp>
2025-04-16 14:39:56 +0900

    ウィンドウを追加しました

--- main.cpp
+++ main.cpp
@@ -1,12 +1,64 @@
 #include <Windows.h>
+#include <cstdint>
 
-// windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	// 出力ウィンドウへの文字出力
-	OutputDebugStringA("Hellp,DirectX!\n");
-
-	return 0;
-
-
-
+// ウィンドウプロシージャ
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	// メッセージに応じてゲーム固有の処理を行う
+	switch (msg) {
+	// ウィンドウが破棄された
+	case WM_DESTROY:
+		// OSに対して、アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+	// 標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
+
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) 
+{
+	WNDCLASS wc{};
+
+	wc.lpfnWndProc = WindowProc;
+
+	wc.lpszClassName = L"CG2WnidowClas";
+
+	wc.hInstance = GetModuleHandle(nullptr);
+
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+	RegisterClass(&wc);
+
+
+	// クライアントの領域サイズ
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
+
+	// ウィンドウサイズを表す構造体にクラインと領域を入れる
+	RECT wrc = {0, 0, kClientWidth, kClientHeight};
+
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	// ウィンドウの生成
+	HWND hwnd = CreateWindow(wc.lpszClassName, L"CG2", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, wrc.right - wrc.left, wrc.bottom - wrc.top, nullptr, nullptr, wc.hInstance, nullptr); 
+
+	// ウィンドウを表示する
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg{};
+	// ウィンドウの×ボタンが押されるまでループ
+	while (msg.message != WM_QUIT) {
+		// Windowにメッセージが着てたら最優先で処理される
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		} else {
+		
+		}
+	}
+
+	return 0; 
+}
+
+
