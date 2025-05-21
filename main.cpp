463135b6a2a0b4ec1cc7690b28607ddbd67f46c6
takeuti2319 <k024g0068@g.neec.ac.jp>
2025-05-21 17:14:01 +0900

    p15まで完了

--- main.cpp
+++ main.cpp
@@ -756,11 +756,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	MSG msg{};
 	// ウィンドウの×ボタンが押されるまでループ
 	while (msg.message != WM_QUIT) {
+
+		ImGui_ImplDX12_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+
 		// Windowにメッセージが着てたら最優先で処理される
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		} else {
+
+			// upData
+			ImGui::ShowDemoWindow();
+			ImGui::Render();
+
 			// ゲーム処理
 
 			// ================================
