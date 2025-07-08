5bafd6595aee161e7576a3d795ebb86ea9b69542
takeuti2319 <k024g0068@g.neec.ac.jp>
2025-07-08 10:34:13 +0900

    05_04課題

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -32,7 +32,10 @@ PixelShaderOutput main(VertexShaderOutput input)
     float32_t4 textureColor = gTexture.Sample(gSampler,input.texcoord);
     if (gMaterial.enableLighting != 0)
     {
-        float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+        //float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+        
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
         output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.
     intensity;
     }
