f771378b4416ac78df84be7a0e2496d551d7f943
takeuti2319 <k024g0068@g.neec.ac.jp>
2026-01-07 10:05:58 +0900

    名前変更

--- main.cpp
+++ main.cpp
@@ -643,7 +643,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ウィンドウの生成
 	HWND hwnd = CreateWindow(
 	    wc.lpszClassName,     // 利用するクラス名
-	    L"CG2",               // タイトルバーの文字
+	    L"LE2E_12_タケウチ_ハルカ",               // タイトルバーの文字
 	    WS_OVERLAPPEDWINDOW,  // よく見るウィンドウスタイル
 	    CW_USEDEFAULT,        // 表示X座標
 	    CW_USEDEFAULT,        // 表示Y座標
